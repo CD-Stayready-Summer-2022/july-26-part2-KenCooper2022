fe5105e13bea33fc6e443755ef928c69d46a089d
Ken Cooper <kennethsecooper@gmail.com>
2022-07-26 10:49:35 -0400

    works but ran into a problem I hope Prof Tariq can answer

--- src/main/java/cd/get/ready/algorithms/CountDuplicates.java
+++ src/main/java/cd/get/ready/algorithms/CountDuplicates.java
@@ -1,21 +1,48 @@
 package cd.get.ready.algorithms;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
      * @param input
      * @return
      */
-    public Integer countDuplicates(String[] input){
-        return null;
+    public Integer countDuplicates(String[] input) {
+        HashMap<String, Integer> numbersAmount = new HashMap<String, Integer>();
+        int count = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (!numbersAmount.containsKey(input[i])) {
+                numbersAmount.put(input[i], 1);
+            } else {
+                numbersAmount.put(input[i], numbersAmount.get(input[i]) + 1);
+            }
+        }
+        for (Map.Entry<String, Integer> set :
+                numbersAmount.entrySet()) {
+            // Printing all elements of a Map
+            if(set.getValue()>1){
+                count++;
+            }
+        }
+
+            return count;
     }
 
-    /**
-     * Example of overloading a function that accepts a Array of Integers
-     * @param input
-     * @return
-     */
-    public Integer countDuplicates(Integer[] input){
-        return null;
+        /**
+         * Example of overloading a function that accepts a Array of Integers
+         * @param input
+         * @return
+         */
+        public Integer countDuplicates(Integer[]input){
+            String str = Arrays.toString(input)
+                    .replaceAll("\\s+", "");
+            String[] strArray = str.substring(1, str.length() - 1)
+                    .split(",");
+           return countDuplicates(strArray);
+
+        }
     }
-}
+
